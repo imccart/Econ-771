bbac71108213ee8287b59db9026216006c917318
Ian McCarthy <ianmccarthy.econ@gmail.com>
2020-07-17 17:12:36 -0400

    begin exercise 2 solutions

---
+++ solutions/exercise2/RunRender.R
@@ -0,0 +1,37 @@
+
+# Meta --------------------------------------------------------------------
+
+## Title:         Econ 771: Exercise 2 Solutions
+## Author:        Ian McCarthy
+## Date Created:  7/17/2020
+## Date Edited:   7/17/2020
+## Description:   This file renders/runs all relevant R code for the project
+
+
+# Preliminaries -----------------------------------------------------------
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stargazer, knitr, kableExtra,
+               lfe, modelr)
+
+
+
+# Load data ------------------------------------------------------------
+
+bench.data <- 
+pen.data <- 
+ahrf.data <- 
+
+
+# Call analysis and set workspace for knitr -------------------------------
+source('analysis.R')
+rm(list=c("data", "data.ind", "households", "data.hh", "zip3.choices",
+          "plan.data", "product.definitions", "final.data", "hh.language"))
+save.image("workspace.Rdata")
+
+
+
+# Run abstract markdown ---------------------------------------------------
+rmarkdown::render(input = 'solutions.Rmd',
+                  output_format = 'all',
+                  output_file ='Exercise2_Answers')
